f99923eb20ca4042ba4b8b9ff3c52f229a5f3930
Marcelo <marcelocalenga08@gmail.com>
2026-08-31 10:31:17 +0100

    Add Program to print even numbers from 2 to 100

---
+++ 1059.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace _1059
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i=2; i <=100; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+

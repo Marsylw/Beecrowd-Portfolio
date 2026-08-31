77968fc2b181c3c0cd8cd94218c97128e88095c8
Marcelo <marcelocalenga08@gmail.com>
2026-08-31 03:18:38 +0100

    Add Program class to calculate distance in minutes

---
+++ 1016.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace _1016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int y, distance;
+            y = Convert.ToInt32(Console.ReadLine());
+
+            distance = y * 2;
+            
+            Console.WriteLine(distance + " minutos");
+
+            Console.ReadLine();
+        }
+    }
+}
+

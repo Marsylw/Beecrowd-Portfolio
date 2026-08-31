e5fb9e012c6e436fe8cc12f2bed599bf2b538824
Marcelo <marcelocalenga08@gmail.com>
2026-08-31 10:44:34 +0100

    Add URI class with main method for output format

---
+++ 1096.cs
@@ -0,0 +1,28 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+int i = 1;
+int primeiro = 0;
+int j = 7;
+
+while (true)
+{
+    if (i >9) { break; }
+    Console.WriteLine($"I={i} J={j}");
+
+    primeiro++;
+    j--;
+
+    if (primeiro == 3)
+    {
+        i += 2; j = 7; primeiro = 0;
+    }
+    
+}
+
+    }
+
+}

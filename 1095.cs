7f72e8c1f292c127b1083f6bccb0bcd30d8e5c73
Marcelo <marcelocalenga08@gmail.com>
2026-08-31 10:45:29 +0100

    Add URI class with Main method for output

---
+++ 1095.cs
@@ -0,0 +1,19 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+       int n = 1;
+           
+
+for (int i = 60; i >=0; i-=5)
+{
+    Console.WriteLine($"I={n} J={i}");
+
+    n += 3;
+    
+}
+    }
+
+}

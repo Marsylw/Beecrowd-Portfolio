0d01c3fdcf68e4cf28eb148702d1b25e41941ea3
Marcelo <marcelocalenga08@gmail.com>
2026-08-31 10:35:27 +0100

    Implement month name output based on user input
    This program reads a number from the user and outputs the corresponding month name.

---
+++ 1052.cs
@@ -0,0 +1,65 @@
+using System;
+
+namespace _1052
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            number = int.Parse(Console.ReadLine());
+
+            if (number == 1)
+            {
+                Console.WriteLine("January");
+            }
+            else if (number == 2)
+            {
+                Console.WriteLine("February");
+            }
+            else if (number == 3)
+            {
+                Console.WriteLine("March");
+            }
+            else if (number == 4)
+            {
+                Console.WriteLine("April");
+            }
+            else if (number == 5)
+            {
+                Console.WriteLine("May");
+            }
+            else if (number == 6)
+            {
+                Console.WriteLine("June");
+            }
+            else if (number == 7)
+            {
+                Console.WriteLine("July");
+            }
+            else if (number == 8)
+            {
+                Console.WriteLine("August");
+            }
+            else if (number == 9)
+            {
+                Console.WriteLine("September");
+            }
+            else if (number == 10)
+            {
+                Console.WriteLine("October");
+            }
+            else if (number == 11)
+            {
+                Console.WriteLine("November");
+            }
+            else if (number == 12)
+            {
+                Console.WriteLine("December");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+

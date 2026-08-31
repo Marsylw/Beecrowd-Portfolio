c6c238116f1671c5c45468ce8160a924940aef39
Marcelo <marcelocalenga08@gmail.com>
2026-08-31 03:06:22 +0100

    Add Program class to calculate difference

---
+++ 1007.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace UriMaster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C, D, diferenca;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+            C = int.Parse(Console.ReadLine());
+            D = int.Parse(Console.ReadLine());
+
+            diferenca = (A * B) - (C * D);
+            Console.WriteLine("DIFERENCA = " + diferenca);
+
+
+
+            
+        }
+    }
+}
+

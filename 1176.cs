984f6f15f082d19cc5f6f13672ec94144f2ea1d0
Marcelo <marcelocalenga08@gmail.com>
2026-09-04 16:35:26 +0100

    Implement Fibonacci sequence calculation

---
+++ 1176.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int t = int.Parse(Console.ReadLine());
+
+        
+        long[] fib = new long[61];
+        fib[0] = 0;
+        fib[1] = 1;
+        for (int i = 2; i <= 60; i++)
+            fib[i] = fib[i - 1] + fib[i - 2];
+
+        for (int i = 0; i < t; i++)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Fib({n}) = {fib[n]}");
+        }
+    }
+}
